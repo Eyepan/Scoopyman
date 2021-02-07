46767fe6fec6e46c93c79fb8384c6d6d0561a39e
Eyepan <iamteoggy@gmail.com>
2021-02-07 17:51:52 +0530

    lmao commit

---
+++ src/pacman.py
@@ -0,0 +1,74 @@
+from sys import argv
+from os import system
+arguments = list(argv)
+
+documentation = '''Welcome to SCOOPYMAN
+
+FLAGS           USE CASE
+-               Show this page. Yeah that's how you got here
+-S              Install an/multiiple apps
+-Syu            Update the system and install apps (if you want tho)
+-Q              List all apps installed (You can also grep this if you want to search for a single app)
+-Qe             Runs scoop which for your app of choice
+-Ss             Search online for your app
+-R              Removes/Uninstalls an app
+
+Anything else   INVALID OPTION and you also get this file as a ball of shame!'''
+
+# to remove the pacman.py argument
+arguments.pop(0)
+
+# TODO: CHECK IF RUNNING IN SUDO
+# SCOOP INSTALL
+if arguments[0] == '-S':
+    arguments.pop(0)
+    print("Installing", *arguments)
+    for i in arguments:
+        system(f"scoop install {i} --global")
+
+#SCOOP UPDATE ALL / SCOOP INSTALL
+elif arguments[0] == '-Syu':
+    arguments.pop(0)
+    print("Updating all apps...")
+    system("scoop update *")
+    print("Running scoop status")
+    system("scoop status")
+    for i in arguments:
+        print("Installing", *arguments)
+        system(f"scoop install {i} --global")
+    
+#SCOOP LIST
+elif arguments[0] == '-Q':
+    print("Listing all applications...")
+    system("scoop list")
+
+#SCOOP SEARCH INSTALLED
+elif arguments[0] == '-Qe':
+    arguments.pop(0)
+    print("WARN Scoop which is not exactly working as intended. I am trying to fix that damn issue")
+    print("Searching for:", *arguments)
+    for i in arguments:
+        system(f"scoop which {i}")
+
+
+#SCOOP SEARCH
+elif arguments[0] == '-Ss':
+    arguments.pop(0)
+    print("Searching for:", *arguments)
+    for i in arguments:
+        system(f"scoop search {i}")
+
+
+#SCOOP UNINSTALL
+elif arguments[0] == '-R':
+    arguments.pop(0)
+    print("Uninstalling:", *arguments)
+    for i in arguments:
+        system(f"scoop uninstall {i} --global")
+
+elif arguments[0] == '-' or arguments[0] == '--help' or arguments[0] == '-h':
+    print(documentation)
+
+else:
+    print("INVALID OPTION")
+    print(documentation)
---
+++ src/scoopymaninstaller.py
@@ -0,0 +1,38 @@
+from os import environ, system
+
+print("MAKE SURE YOU ARE RUNNING THIS PYTHON FILE IN AN ELEVATED POEWRSHELL")
+system("pause")
+# CHECK WHETHER SCOOP IS INSTALLED
+
+
+def installscoop():
+    print("SETTING EXECUTION POLICY, PRESS A WHEN PROMPTED")
+    system("Set-ExecutionPolicy RemoteSigned -scope CurrentUser")
+    print("INSTALLING SCOOP")
+    system("iwr -useb get.scoop.sh | iex")
+    print("SCOOP SUCCESSFULLY INSTALLED")
+
+try:
+    print(f"SCOOP IS INSTALLED IN {environ['SCOOP']}")
+except KeyError:
+    print(f"SCOOP IS NOT INSTALLED. INSTALLING SCOOP RIGHT NOW")
+    installscoop()
+
+# SCOOP SHOULD INSTALL SUCCESSFULLY
+
+# NOW SETTING UP PACMAN
+
+print("PRESS ANY KEY TO INSTALL pacman.py")
+input()
+system("pip install wheel nuitka")
+system("nutika src\\pacman.py")
+system(f"Copy-Item src\\pacman.exe {environ['SCOOP']}\\shims\\pacman.exe")
+
+print("Cleaning up build files...")
+system("Remove-Item python39.dll")
+system("Remove-Item pacman.build -Recurse")
+
+# CHECKING IF INSTALLATION WENT SUCCESSFULLY
+print("UPDATING AND SETTING UP BASIC APPS")
+system("pacman -S 7zip git")
+system("pacman -Syu gsudo aria2 grep python")
